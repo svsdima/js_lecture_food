ee6c0ad1393f574839e6dffd0fccbbb5d623e097
Dmitriy <svsdima1995@mail.ru>
2020-10-13 15:57:13 +0300

    Создаём таймер обратного отсчёта на сайте

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,11 @@
-/* Создаём табы в новом проекте */
+
 
 'use strict';
 
 /* 1) Назначение глобального обработчика событий, который называется DOMContentLoaded */
 window.addEventListener('DOMContentLoaded', () => {
+    /* Создаём табы в новом проекте */
+
     /* Внутри него будут все остальные скрипты, которые будут созданы в этом проекте */
 
     /* Алгоритм действий:
@@ -54,4 +56,72 @@ window.addEventListener('DOMContentLoaded', () => {
             });
         }
     });
+
+    // -------------------------------------------------------------------------------------------------------------
+    /* Создаём таймер обратного отсчёта на сайте */
+
+ /* - Должна быть функция, которая будет устанавливать таймер
+    - Должна быть функция, которая будет определять разницу между временем
+    - Должна быть функция, которая будет обновлять наш таймер  */
+
+    /* Отправная точка */
+    const deadline = '2020-11-13';
+
+    function getTimeRemaining(endtime) {
+        /* Получаем кол-во миллисекунд, которая будет в конечном времени (до которого нужно досчитать) - новая дата. Получаем разницу в миллисекундах */
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+        /* Кол-во дней, которые будут отображаться в таймере */
+        /* Math.floor - округление до ближайшего целого */
+              days = Math.floor(t / (1000 * 60 * 60 * 24)),
+              hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+              minutes = Math.floor((t / 1000 / 60) % 60),
+              seconds = Math.floor((t / 1000) % 60);
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds,
+        };
+    }
+
+    /* Добавляем 0 в значении меньше 10 (09, 08, 07) */
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
+    /* Устанавливаем часы на страницу */
+    function setClock(selector, endtime) {
+       const timer = document.querySelector(selector),
+             days = timer.querySelector('#days'),
+             hours = timer.querySelector('#hours'),
+             minutes = timer.querySelector('#minutes'),
+             seconds = timer.querySelector('#seconds'),
+             timeInterval = setInterval(updateClock, 1000);
+
+        /* Запускаем заранее обновление часов, чтобы избежать загрузки на самой странице */
+        updateClock();
+        
+        /* Обновление часов */
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+
+        }
+    }
+
+    setClock('.timer', deadline);
 });
