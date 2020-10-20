e484923bc3c15716a3d384ef50b62ccd3bd39c3a
Dmitriy <svsdima1995@mail.ru>
2020-10-20 12:16:44 +0300

    Реализация скрипта отправки данных на сервер (для работы нужно запускать на Локальном сервере

--- dist/js/script.js
+++ dist/js/script.js
@@ -91,12 +91,11 @@
   !*** ./src/js/main.js ***!
   \************************/
 /*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
+/***/ (function(module, exports) {
 
-"use strict";
+// 'use strict';
 
 /* 1) Назначение глобального обработчика событий, который называется DOMContentLoaded */
-
 window.addEventListener('DOMContentLoaded', () => {
   /* Создаём табы в новом проекте */
 
@@ -338,7 +337,53 @@ window.addEventListener('DOMContentLoaded', () => {
 
   new MenuCard("img/tabs/vegy.jpg", "vegy", 'Меню "Фитнес"', 'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!', 9, '.menu .container').render();
   new MenuCard("img/tabs/elite.jpg", "elite", 'Меню “Премиум”', 'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!', 12, '.menu .container', 'menu__item', 'big').render();
-  new MenuCard("img/tabs/post.jpg", "post", 'Меню "Постное"', 'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.', 6, '.menu .container', 'menu__item', 'big').render();
+  new MenuCard("img/tabs/post.jpg", "post", 'Меню "Постное"', 'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.', 6, '.menu .container', 'menu__item', 'big').render(); // -------------------------------------------------------------------------------------------------------------
+
+  /* Реализация скрипта отправки данных на сервер*/
+
+  /*  Формы */
+
+  const forms = document.querySelectorAll('form');
+  const message = {
+    loading: 'Загрузка',
+    succes: 'Спасибо! Скоро мы с вами свяжемся',
+    failure: 'Что-то пошло не так...'
+  };
+  forms.forEach(item => {
+    postData(item);
+  });
+
+  function postData(form) {
+    form.addEventListener('submit', e => {
+      e.preventDefault();
+      const statusMessage = document.createElement('div');
+      statusMessage.classList.add('status');
+      statusMessage.textContent = message.loading;
+      form.append(statusMessage);
+      const request = new XMLHttpRequest();
+      request.open('POST', 'server.php');
+      request.setRequestHeader('Content-type', 'application/json');
+      const formData = new FormData(form);
+      const object = {};
+      formData.forEach(function (value, key) {
+        object[key] = value;
+      });
+      const json = JSON.stringify(object);
+      request.send(json);
+      request.addEventListener('load', () => {
+        if (request.status === 200) {
+          console.log(request.response);
+          statusMessage.textContent = message.succes;
+          form.reset();
+          setTimeout(() => {
+            statusMessage.remove();
+          }, 4000);
+        } else {
+          statusMessage.textContent = message.failure;
+        }
+      });
+    });
+  }
 });
 
 /***/ })
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 
 
-'use strict';
+// 'use strict';
 
 /* 1) Назначение глобального обработчика событий, который называется DOMContentLoaded */
 window.addEventListener('DOMContentLoaded', () => {
@@ -264,4 +264,58 @@ window.addEventListener('DOMContentLoaded', () => {
     ).render();
 
 
+    // -------------------------------------------------------------------------------------------------------------
+    /* Реализация скрипта отправки данных на сервер*/
+
+   /*  Формы */
+   const forms = document.querySelectorAll('form');
+
+   const message = {
+    loading: 'Загрузка',
+    succes: 'Спасибо! Скоро мы с вами свяжемся',
+    failure: 'Что-то пошло не так...'
+   };
+
+   forms.forEach(item => {
+       postData(item);
+   });
+
+   function postData(form) {
+       form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const statusMessage = document.createElement('div');
+        statusMessage.classList.add('status');
+        statusMessage.textContent = message.loading;
+        form.append(statusMessage);
+
+        const request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+
+        request.setRequestHeader('Content-type', 'application/json');
+        const formData = new FormData(form);
+
+        const object = {};
+        formData.forEach(function (value, key) {
+            object[key] = value;
+        });
+
+        const json = JSON.stringify(object);
+
+        request.send(json);
+
+        request.addEventListener('load', () => {
+            if (request.status === 200) {
+                console.log(request.response);
+                statusMessage.textContent = message.succes;
+                form.reset();
+                setTimeout(() => {
+                    statusMessage.remove();
+                }, 4000);
+            } else {
+                statusMessage.textContent = message.failure;
+            }
+        });
+       });
+   }
 });
